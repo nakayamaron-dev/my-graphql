fe6feab55d7f2cc7ec6af53590964878499ac252
Ryo Nakayama <nakayamadevelop@gmail.com>
2022-05-14 18:07:18 +0900

    graphqlのコードを個別ファイルに分離

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React, { Component } from "react";
 import { ApolloProvider, Query } from "react-apollo";
 import client from "./client";
-import { gql } from "graphql-tag";
-
-const ME = gql`
-  query me {
-    user(login: "nakayamaron-dev") {
-      name
-      avatarUrl
-    }
-  }
-`;
+import { ME } from "./graphql";
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div>Hello GraphQL</div>
-
         <Query query={ME}>
           {({ loading, error, data }) => {
             if (loading) return "Loading...";
