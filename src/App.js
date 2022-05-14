0634e1655a386807fc36424e0e85376edb572fa8
Ryo Nakayama <nakayamadevelop@gmail.com>
2022-05-14 18:02:02 +0900

    graphQLクライアントを実装

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import "./App.css";
+import React, { Component } from "react";
+import { ApolloProvider, Query } from "react-apollo";
+import client from "./client";
+import { gql } from "graphql-tag";
 
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+const ME = gql`
+  query me {
+    user(login: "nakayamaron-dev") {
+      name
+      avatarUrl
+    }
+  }
+`;
 
-function App() {
-  return <div>hello graphql</div>;
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div>Hello GraphQL</div>
+
+        <Query query={ME}>
+          {({ loading, error, data }) => {
+            if (loading) return "Loading...";
+            if (error) return `Error! ${error.message}`;
+
+            return <div>{data.user.name}</div>;
+          }}
+        </Query>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
