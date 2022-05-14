c970f8c9168673d9e8fad05d2312de35ec1d8e94
Ryo Nakayama <nakayamadevelop@gmail.com>
2022-05-14 18:18:38 +0900

    search repogitory queryを実装

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from "react";
 import { ApolloProvider, Query } from "react-apollo";
 import client from "./client";
-import { ME } from "./graphql";
+import { SEARCH_REPOSITORIES } from "./graphql";
+
+const VARIABLES = {
+  first: 5,
+  after: null,
+  last: null,
+  before: null,
+  query: "フロントエンドエンジニア",
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = VARIABLES;
+  }
+
   render() {
+    const { query, first, last, before, after } = this.state;
+
     return (
       <ApolloProvider client={client}>
-        <Query query={ME}>
+        <Query
+          query={SEARCH_REPOSITORIES}
+          variables={{ query, first, last, before, after }}
+        >
           {({ loading, error, data }) => {
             if (loading) return "Loading...";
             if (error) return `Error! ${error.message}`;
-
-            return <div>{data.user.name}</div>;
+            console.log(data);
+            return <div></div>;
           }}
         </Query>
       </ApolloProvider>
